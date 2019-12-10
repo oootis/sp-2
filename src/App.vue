196ab356a3898a1633d64cdc0252ae055bf60f8b
otis <peijiang96@outlook.com>
2019-12-10 21:24:31 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <appNewQuote @quoteAdded="newQuote"></appNewQuote>
+    <appQuoteGrid :quotes="quotes" @quoteDeleted="deleteIt"></appQuoteGrid>
+    <appHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes" v-if="quotes.length > 0"></appHeader>
+      <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuoteGrid from './components/QuoteGrid'
+import NewQuote from './components/NewQuote'
+import Header from './components/Header'
 
 export default {
-  name: 'app',
+  data: function(){
+    return {
+      quotes: ['Destiny is not a matter of chance , it is a matter of choice'],
+      maxQuotes: 10
+    }
+  },
+  methods:{
+    newQuote(quote){
+      if(this.quotes.length >= this.maxQuotes){
+       return alert('DELETE FIRST!');
+      }
+      this.quotes.push(quote);
+    },
+    deleteIt(index){
+      this.quotes.splice(index, 1)
+    }
+
+
+  },
   components: {
-    HelloWorld
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
   }
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+      <appQuote v-for="(quote,index) in quotes" :key="index" @click.native="deleteQuote(index)">{{quote}}</appQuote>
+  </div>                                                     <!-- 给vue组件标签绑定事件时候，必须加上native ，否则会认为监听的是来自组件自定义的事件-->
+</template>
+
+<script>
+import Quote from './Quote';
+export default {
+    props: ['quotes'],
+    components: {
+        appQuote: Quote
+    },
+    methods:{
+        deleteQuote(index){
+            this.$emit('quoteDeleted', index);
+        }
+    }
+
+}
+</script>
+
+<style>
+
+
+</style>
